fe15f7789bbc3a115dc1a64fc587e49c662086b9
HDANILO <helton.danilo@gmail.com>
2019-01-22 04:37:59 -0300

    removed bundle mapping

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ module.exports = {
     module: {
         rules: [
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
     }
 }
